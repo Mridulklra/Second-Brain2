13b0cd25b9713aac8583e469635cac9c4c09c399
Mridul Kalra <mridulkalra700@gmail.com>
2025-12-04 13:21:24 +0530

    first commit

---
+++ Server/src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || '');
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB");
+    console.error(error);
+    process.exit(1);
+  }
+};
